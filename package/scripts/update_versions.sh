d2a07b0dcbfe7b3beff53d53cb77e03e7bdc2002
PeterFarber <farberpete@gmail.com>
2024-02-23 16:33:03 -0500

    Small Refactor

--- package/scripts/update_versions.sh
+++ package/scripts/update_versions.sh
@@ -1,4 +1,8 @@
+cp ./npm/* ./dist
+cp ./npm/README.md ../
+
 B=`awk -F'"' '/"version": ".+"/{ print $4; exit; }' package.json`
 echo 'Updating Versions To '${B}'...'
 sed -i '' 's/$version/'${B}'/g' dist/README.md
 sed -i '' 's/$version/'${B}'/g' dist/package.json
+sed -i '' 's/$version/'${B}'/g' ../README.md
